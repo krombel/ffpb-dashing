f891721a01d131d33c7b25ea5c0bda608159d9c1
Krombel <mail@krombel.de>
2014-10-02 00:09:52 +0200

    added support for highscore-achieving-date (updating-job)

--- jobs/fill_Count_info.rb
+++ jobs/fill_Count_info.rb
@@ -5,19 +5,20 @@ aktClientCount = 0
 aktNodeCount = 0
 maxClientCount = 521
 maxNodeCount = 347
-mychanged = 42 # unimportant, which value
+
+lastAktValueChanged = 42 # unimportant, which value
+lastMaxNodeCountDate = 0
+lastMaxClientCountDate = 0
 
 SCHEDULER.every '5s', :first_in => 0 do
 	# update as fast as possible after the update of the counts-Skript. And then wait for the next value.
- changedfile = open('../freifunk-counts/changevalue.txt') { |f| f.read }
- if mychanged != changedfile
+ aktValueChanged = open('../freifunk-counts/changevalue.txt') { |f| f.read }
+ if lastAktValueChanged != aktValueChanged
   aktClientCount2 = aktClientCount
   aktNodeCount2 = aktNodeCount
  
   aktNodeCount = open('../freifunk-counts/Nodecount/aktNodecount.txt') { |f| f.read }
-  maxNodeCount = open('../freifunk-counts/Nodecount/maxNodecount.txt') { |f| f.read }
   aktClientCount = open('../freifunk-counts/Clientcount/aktClientcount.txt') { |f| f.read }
-  maxClientCount = open('../freifunk-counts/Clientcount/maxClientcount.txt') { |f| f.read }
 
   # only show the difference in percent, if the value has changed
   if aktClientCount2 == aktClientCount
@@ -31,11 +32,22 @@ SCHEDULER.every '5s', :first_in => 0 do
   else
   	send_event('aktNodeCount', { current: aktNodeCount, last: aktNodeCount2 })
   end
-  
-  send_event('maxClientCount', { current: maxClientCount})
-  send_event('maxNodeCount', { current: maxNodeCount})
-  mychanged = changedfile
- end
+  lastAktValueChanged = aktValueChanged
+ end # if aktValueChanged
+ 
+ maxNodeCountDate = open('../freifunk-counts/Nodecount/maxNodecount.date.txt') { |f| f.read }
+ if lastMaxNodeCountDate != maxNodeCountDate
+  maxNodeCount = open('../freifunk-counts/Nodecount/maxNodecount.txt') { |f| f.read }
+  send_event('maxNodeCount', { current: maxNodeCount, archievedAt: maxNodeCountDate })
+  lastMaxNodeCountDate = maxNodeCountDate
+ end # if maxNodeCount changed 
+ 
+ maxClientCount = open('../freifunk-counts/Clientcount/maxClientcount.txt') { |f| f.read }
+ if lastMaxClientCountDate != maxClientCountDate
+  maxClientCountDate = open('../freifunk-counts/Clientcount/maxClientcount.date.txt') { |f| f.read }
+  send_event('maxClientCount', { current: maxClientCount, archievedAt: maxClientCountDate })
+  lastMaxClientCountDate = maxClientCountDate
+ end # if maxClientCount changed
 end
 
 #require 'open-uri'
