c0be594183bae4907e5c5d901e399fafb8b12d55
Krombel <mail@krombel.de>
2014-11-12 20:06:03 +0100

    added "dashing-send-list"

---
+++ dashing-send-list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+host="localhost:3030"
+auth_token="bitnhmlj47hamrftxkiug"
+
+metric=$1
+shift
+
+items="["
+while (( "$#" )); do
+	x=$1;shift
+	y=$1;shift
+
+	items="${items}{ \"label\": \"$x\", \"value\": \"$y\" }"
+	if (( "$#" )); then
+		items="${items}, "
+	fi
+done
+items="${items}]"
+
+info="{ \"auth_token\": \"$auth_token\", \"items\": $items }"
+echo $info
+curl -d "$info" http://$host/widgets/$metric
